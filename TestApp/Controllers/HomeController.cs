fa86266e7dffe1c72acfdd4a3f8c3a20c3853461
Jorge Tarantino <jorge.tarantino@globant.com>
2018-01-03 14:03:36 -0300

    Add project files.

---
+++ TestApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using TestApp.Models;
+
+namespace TestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Events()
+        {
+
+            var list = SetupMockupData();
+            ViewBag.Message = "Your event page.";
+
+            return View();
+        }
+
+        private List<EventModel> SetupMockupData()
+        {
+
+            var list = new List<EventModel>();
+            for (var i = 0; i < 1000; i++)
+            {
+                var e = new EventModel();
+                list.Add(e);
+            }
+            return list;
+
+        }
+    }
+}
---
+++ TestApp/Models/EventModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestApp.Models
+{
+    public class EventModel
+    {
+
+        public string EventTitle; 
+
+        public string Technology;
+        
+        public DateTime StartingDate;
+
+        public string RegistrationLink;
+
+
+    }
+}
